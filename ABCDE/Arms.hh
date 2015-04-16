caec5b6175a2d8e148bb3dbc021ff4f5e6ef7f30
Ashkan Dehghan <ash.dehghan@gmail.com>
2015-04-16 10:08:51 -0400

    got all period size for Hex and Lam

--- ABCDE/Arms.hh
+++ ABCDE/Arms.hh
@@ -1,7 +1,7 @@
 void Arms(int Numb_of_Arms, double *Ns){
 
 
-  if(Numb_of_Arms==1){
+  if(Numb_of_Arms==1){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=1;  // A2                                                                                                                      
     Ns[2]=1;  // A3                                                                                                             
     Ns[3]=1;  // A4                                                                                                   
@@ -11,8 +11,8 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[7]=1;  // B4 
     // Setting the size of the period for the system
     Lam_Period=3.45;
-    Hex_Period=3.8;
-  }else if(Numb_of_Arms==2){
+    Hex_Period=3.75;
+  }else if(Numb_of_Arms==2){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=Ns[0];  // A2                                                                                                                      
     Ns[2]=1;  // A3                                                                                                                      
     Ns[3]=1;  // A4                                                                                                                      
@@ -22,8 +22,8 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[7]=1;  // B4
    // Setting the size of the period for the system
     Lam_Period=2.72;
-    Hex_Period=2.68;
-  }else if(Numb_of_Arms==3){
+    Hex_Period=2.98;
+  }else if(Numb_of_Arms==3){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=Ns[0];  // A2                                                                                                                      
     Ns[2]=Ns[0];  // A3                                                                                                                      
     Ns[3]=1;  // A4                                                                                                                      
@@ -33,8 +33,8 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[7]=1;  // B4
     // Setting the size of the period for the system
     Lam_Period=2.32;
-    Hex_Period=2.19;
-  }else if(Numb_of_Arms==4){
+    Hex_Period=2.49;
+  }else if(Numb_of_Arms==4){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=Ns[0];  // A2                                                                                                                      
     Ns[2]=Ns[0];  // A3                                                                                                                      
     Ns[3]=Ns[0];  // A4                                                                                                                      
@@ -44,7 +44,7 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[7]=100-Ns[0];  // B4
     // Setting the size of the period for the system
     Lam_Period=2.0;
-    Hex_Period=1.9;
+    Hex_Period=2.2;
   }else{
     // Error
     std::cout<<"The number of arms you have selected is incorrect! (Arms.hh)"<<std::endl;
