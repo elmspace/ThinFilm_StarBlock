8f0c1127c84c7bca208b87d94d81de9adf0a8fd0
Ash <ash.dehghan@gmail.com>
2015-05-06 23:22:01 -0400

    got the lam period for xAB=20

--- ABCDE/Arms.hh
+++ ABCDE/Arms.hh
@@ -10,9 +10,13 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[6]=1;  // B3              
     Ns[7]=1;  // B4 
     // Setting the size of the period for the system
-    Lam_Period=3.45;
-    Hex_Period=3.75;
-    BCC_Period=4.5;
+    //Lam_Period=3.45; //xAB 14
+    //Hex_Period=3.75; //xAB 14
+    //BCC_Period=4.5; //xAB 14
+
+    Lam_Period=3.83; //xAB 20
+    Hex_Period=4.13; //xAB 20
+    BCC_Period=5.00; //xAB 20 
   }else if(Numb_of_Arms==2){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=Ns[0];  // A2                                                                                                                      
     Ns[2]=1;  // A3                                                                                                                      
@@ -21,10 +25,14 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[5]=100-Ns[0];  // B2                                                                                                                  
     Ns[6]=1;  // B3                                                                                                                  
     Ns[7]=1;  // B4
-   // Setting the size of the period for the system
-    Lam_Period=2.72;
-    Hex_Period=2.98;
-    BCC_Period=3.33;
+    // Setting the size of the period for the system
+    //Lam_Period=2.72; //xAB14
+    //Hex_Period=2.98; //xAB14
+    //BCC_Period=3.33; //xAB14
+
+    Lam_Period=2.97; //xAB20
+    Hex_Period=3.25; //xAB20
+    BCC_Period=4.00; //xAB20 
   }else if(Numb_of_Arms==3){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=Ns[0];  // A2                                                                                                                      
     Ns[2]=Ns[0];  // A3                                                                                                                      
@@ -34,9 +42,13 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[6]=100-Ns[0];  // B3                                                                                                                  
     Ns[7]=1;  // B4
     // Setting the size of the period for the system
-    Lam_Period=2.32;
-    Hex_Period=2.49;
-    BCC_Period=2.77;
+    //Lam_Period=2.32; //xAB14
+    //Hex_Period=2.49; //xAB14
+    //BCC_Period=2.77; //xAB14
+
+    Lam_Period=2.49; //xAB20
+    Hex_Period=2.68; //xAB20
+    BCC_Period=3.00; //xAB20 
   }else if(Numb_of_Arms==4){ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     Ns[1]=Ns[0];  // A2                                                                                                                      
     Ns[2]=Ns[0];  // A3                                                                                                                      
@@ -46,9 +58,13 @@ void Arms(int Numb_of_Arms, double *Ns){
     Ns[6]=100-Ns[0];  // B3                                                                                                                 
     Ns[7]=100-Ns[0];  // B4
     // Setting the size of the period for the system
-    Lam_Period=2.0;
-    Hex_Period=2.2;
-    BCC_Period=2.3;
+    //Lam_Period=2.0; //xAB14
+    //Hex_Period=2.2; //xAB14
+    //BCC_Period=2.3; //xAB14
+
+    Lam_Period=2.18; //xAB20
+    Hex_Period=2.39; //xAB20
+    BCC_Period=3.00; //xAB20  
   }else{
     // Error
     std::cout<<"The number of arms you have selected is incorrect! (Arms.hh)"<<std::endl;
